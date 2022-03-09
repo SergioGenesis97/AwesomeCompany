c1c0c02dd3482b439a4e6142c6bf649f5b8eb268
SergioGenesis97 <sergio.bautista@alumnos.udg.mx>
2022-03-08 21:21:15 -0600

    Cambio de importacion de variables

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-export const bodyParser = require('body-parser');
-import './database/connection';
+import Express from 'express';
+const express = Express;
+const app = Express();
 
+import Path from 'path'
+const path = Path;
 
+import BodyParser from 'body-parser';
+const bodyParser = BodyParser;
+
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Settings
 app.set('port', 3000);
@@ -13,7 +19,7 @@ app.set('views', [path.join(__dirname, 'public'),
                   path.join(__dirname, 'views/routes'),
                   path.join(__dirname, 'views/routes/employee'),
                   path.join(__dirname, 'views/partials')]);
-app.engine('html', require('ejs').renderFile);
+app.engine('html', import('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 // Middlewares
@@ -23,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Routes
-app.use(require('../src/views/routes/index'));
+app.use(require('./source/modules/employee/models-empl'));
 
 // Statics files
 app.use(express.static(path.join(__dirname, 'public')));
